f0414b8126582e15ca0ec69cde488956f7331029
sinhashlok <shlokjp@gmail.com>
2024-07-27 01:19:57 +0530

    Merchant App setup

---
+++ apps/merhcant-app/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@repo/shadcn/components/ui/button";
+
+const page = () => {
+  return (
+    <div className="font-bold">
+      Merchant HomePage
+      <Button variant="secondary">Merchant Login</Button>
+    </div>
+  );
+};
+
+export default page;
